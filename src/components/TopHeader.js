8d74314c60bea0c7542880c99de500445c72f73c
zhenGuo27 <someone199079qqq17@gmail.com>
2021-09-02 16:29:20 +0800

    TopHeader - modify Mobile layout

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,10 +1,24 @@
-import { useContext } from "react";
+import $ from "jquery";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { hostPath } from "../actions/sharedConst";
 import AuthContext from "../store/auth-context";
 
 const TopHeader = () => {  
   const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    userlink_dropdown();
+  }, []);
+
+  const userlink_dropdown = () => {
+    $(".top-header .user-menu").on("click", function () {
+      if ($(window).width() < 990) {
+        $(this).next().slideToggle(300);
+        $(this).parent().toggleClass("active");
+      }
+    });
+  };
   
   return (
     <div className="top-header">
@@ -12,10 +26,25 @@ const TopHeader = () => {
         <div className="d-flex justify-content-end">
           <span className="user-menu d-block d-lg-none">
             <i className="anm anm-user-al" aria-hidden="true"></i>
+            <ul class="customer-links list-inline">
+              <li>
+                {!authCtx.isLoggedIn && (
+                  <Link to={hostPath + "/Login"}>Login</Link>
+                )}
+                {authCtx.isLoggedIn && <a onClick={authCtx.logout}>LogOut</a>}
+              </li>
+              <li>
+                {!authCtx.isLoggedIn && (
+                  <Link to={hostPath + "/Register"}>Create Account</Link>
+                )}
+              </li>
+            </ul>
           </span>
           <ul className="customer-links list-inline">
             <li>
-              {!authCtx.isLoggedIn && <Link to={hostPath + "/Login"}>Login</Link>}
+              {!authCtx.isLoggedIn && (
+                <Link to={hostPath + "/Login"}>Login</Link>
+              )}
               {authCtx.isLoggedIn && <a onClick={authCtx.logout}>LogOut</a>}
             </li>
             <li>
